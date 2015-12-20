727723c0138ffd8a4207ae3cc8732908438b1272
anji <sanjaneyulur@gmail.com>
2015-12-20 23:10:32 +0530

    1 line is added

---
+++ Fibonacci.java
@@ -0,0 +1,17 @@
+//Class is defined
+class Fibonacci
+{
+	public static void main(String args[])
+	{		
+		int prev, next, sum, n;
+		prev=next=1;
+		for(n=1;n<=10;n++)
+		{
+			System.out.println(prev);
+			sum=prev+next;
+			prev=next;
+			next=sum;
+		}
+	}
+}
+      
